3ec527aa1fba6c27064a68b2585bb24e3f11d858
DevNitro5 <anishchhetri196@gmail.com>
2023-01-09 11:31:35 +0000

    add KeyBoard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,58 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { HangmanDrawing } from "./components/HangmanDrawing";
+import { HangmanWord } from "./components/HangmanWord";
+import { KeyBoard } from "./components/KeyBoard";
+import words from "./data/wordList.json";
+
+const getWord = () => words[Math.floor(Math.random() * words.length)];
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [wordToGuess, setWordToGuess] = useState(getWord);
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([
+    ...wordToGuess.split("").slice(2),
+    "z",
+  ]);
+
+  const isLoser =
+    guessedLetters.length === 6 && guessedLetters.join("") === wordToGuess;
+
+  const isWinner = true;
+
+  // console.log(isLoser,guessedLetters.length);
 
   return (
     <div className="mt-7 flex flex-col items-center gap-8 mx-auto">
       <HangmanDrawing numberOfGuesses={10} />
+      <HangmanWord
+        reveal={isLoser}
+        wordToGuess={wordToGuess}
+        guessedLetters={guessedLetters}
+      />
+
+      <div style={{ alignSelf: "stretch" }}>
+        <KeyBoard
+          // disabled={isWinner || isLoser}
+          disabled={false}
+          guessedLetters={guessedLetters}
+          wordToGuess={wordToGuess}
+          setGuessedLetters={setGuessedLetters}
+        />
+      </div>
+      <button
+        onClick={() => {
+          setGuessedLetters((p) => [...p, "1"]);
+        }}
+      >
+        Add
+      </button>
     </div>
   );
 }
 
 export default App;
+
+// activeLetters={guessedLetters.filter((letter) =>
+//   wordToGuess.includes(letter)
+// )}
+// inactiveLetters={incorrectLetters}
+// addGuessedLetter={addGuessedLetter}
