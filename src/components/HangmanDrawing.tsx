0cb805ad41d2f7caf31f829bbb574ac86fdd811c
DevNitro5 <anishchhetri196@gmail.com>
2023-01-09 09:19:24 +0000

    Add hangman

---
+++ src/components/HangmanDrawing.tsx
@@ -0,0 +1,41 @@
+const HEAD = (
+  <div className="absolute top-[50px] right-[-30px] rounded-full border-gray-700 border-8 w-14 h-14" />
+);
+
+const BODY = (
+  <div className="w-2 bg-gray-700 absolute -right-1 top-[99px] h-32" />
+);
+
+const RIGHT_ARM = (
+  <div className="arm rotate-[-30deg] origin-bottom-left right-[-85px]" />
+);
+
+const LEFT_ARM = (
+  <div className="arm origin-bottom-right rotate-[30deg] right-[4px]" />
+);
+
+const RIGHT_LEG = (
+  <div className="leg -right-[79px] rotate-[60deg] origin-bottom-left" />
+);
+
+const LEFT_LEG = (
+  <div className="leg right-0 rotate-[-57deg] origin-bottom-right" />
+);
+
+const BODY_PARTS = [HEAD, BODY, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG];
+
+type HangmanDrawingProps = {
+  numberOfGuesses: number;
+};
+
+export function HangmanDrawing({ numberOfGuesses }: HangmanDrawingProps) {
+  return (
+    <div className="relative">
+      {BODY_PARTS.slice(0, numberOfGuesses)}
+      <div className="w-2 h-[3.15rem] bg-gray-700 absolute top-0 right-0 " />
+      <div className="w-48 h-2 bg-gray-700 ml-[7.5rem]" />
+      <div className="w-2 h-72 bg-gray-700 ml-[7.5rem]" />
+      <div className="h-2 w-56 bg-gray-700" />
+    </div>
+  );
+}
