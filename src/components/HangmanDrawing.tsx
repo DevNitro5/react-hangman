01e8ff9e32ab5e60024b4a3e39bf27f0176f1974
DevNitro5 <anishchhetri196@gmail.com>
2023-01-09 11:30:45 +0000

    add keys

--- src/components/HangmanDrawing.tsx
+++ src/components/HangmanDrawing.tsx
@@ -1,25 +1,43 @@
 const HEAD = (
-  <div className="absolute top-[50px] right-[-30px] rounded-full border-gray-700 border-8 w-14 h-14" />
+  <div
+    key="HEAD"
+    className="absolute top-[50px] right-[-30px] rounded-full border-gray-700 border-8 w-14 h-14"
+  />
 );
 
 const BODY = (
-  <div className="w-2 bg-gray-700 absolute -right-1 top-[99px] h-32" />
+  <div
+    key="BODY"
+    className="w-2 bg-gray-700 absolute -right-1 top-[99px] h-32"
+  />
 );
 
 const RIGHT_ARM = (
-  <div className="arm rotate-[-30deg] origin-bottom-left right-[-85px]" />
+  <div
+    key="RIGHT_ARM"
+    className="arm rotate-[-30deg] origin-bottom-left right-[-85px]"
+  />
 );
 
 const LEFT_ARM = (
-  <div className="arm origin-bottom-right rotate-[30deg] right-[4px]" />
+  <div
+    key="LEFT_ARM"
+    className="arm origin-bottom-right rotate-[30deg] right-[4px]"
+  />
 );
 
 const RIGHT_LEG = (
-  <div className="leg -right-[79px] rotate-[60deg] origin-bottom-left" />
+  <div
+    key="RIGHT_LEG"
+    className="leg -right-[79px] rotate-[60deg] origin-bottom-left"
+  />
 );
 
 const LEFT_LEG = (
-  <div className="leg right-0 rotate-[-57deg] origin-bottom-right" />
+  <div
+    key="LEFT_LEG"
+    className="leg right-0 rotate-[-57deg] origin-bottom-right"
+  />
 );
 
 const BODY_PARTS = [HEAD, BODY, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG];
