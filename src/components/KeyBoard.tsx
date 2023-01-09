60b2c49c375427a5b2b0c1567f3d279371f1606b
DevNitro5 <anishchhetri196@gmail.com>
2023-01-09 11:32:14 +0000

    add KeyBoard component

---
+++ src/components/KeyBoard.tsx
@@ -0,0 +1,71 @@
+interface KeyBoardProps {
+  disabled: boolean;
+  activeLetters?: string[];
+  inactiveLetters?: string[];
+  guessedLetters: string[];
+  wordToGuess: string;
+  setGuessedLetters: React.Dispatch<React.SetStateAction<string[]>>;
+}
+
+const KEYS = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+export function KeyBoard(props: KeyBoardProps) {
+  const {
+    activeLetters = ["a", "b"],
+    inactiveLetters = ["c", "d"],
+    disabled,
+    wordToGuess,
+    guessedLetters,
+    setGuessedLetters,
+  } = props;
+
+  return (
+    <div className="grid grid-cols-[repeat(auto-fit,_minmax(75px,_1fr))] gap-2">
+      {KEYS.map((key) => {
+        const wordToGuessIncludes = wordToGuess.includes(key);
+        const guessedLettersIncludes = guessedLetters.includes(key);
+        const isActive = wordToGuessIncludes && guessedLettersIncludes;
+        const isInactive = guessedLettersIncludes && !wordToGuessIncludes;
+
+        return (
+          <button
+            onClick={() => setGuessedLetters((p) => [...p, key])}
+            className={`btn${isActive ? " active" : ""}${
+              isInactive ? " inactive" : ""
+            }`}
+            disabled={isInactive || isActive || disabled}
+            key={key}
+          >
+            {key}
+          </button>
+        );
+      })}
+    </div>
+  );
+}
