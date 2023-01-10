f3d082c874149bb5e5ff8814a508715539fc44a1
DevNitro5 <anishchhetri196@gmail.com>
2023-01-10 09:02:23 +0000

    add keyboard event

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { HangmanDrawing } from "./components/HangmanDrawing";
 import { HangmanWord } from "./components/HangmanWord";
 import { KeyBoard } from "./components/KeyBoard";
@@ -8,51 +8,60 @@ const getWord = () => words[Math.floor(Math.random() * words.length)];
 
 function App() {
   const [wordToGuess, setWordToGuess] = useState(getWord);
-  const [guessedLetters, setGuessedLetters] = useState<string[]>([
-    ...wordToGuess.split("").slice(2),
-    "z",
-  ]);
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
 
-  const isLoser =
-    guessedLetters.length === 6 && guessedLetters.join("") === wordToGuess;
+  const numberOfIncorrectGuesses = guessedLetters.filter(
+    (letter) => !wordToGuess.includes(letter)
+  ).length;
+  const isLoser = numberOfIncorrectGuesses === 6;
 
-  const isWinner = true;
+  console.log(wordToGuess);
+  const isWinner = wordToGuess
+    .split("")
+    .every((word) => guessedLetters.includes(word));
 
-  // console.log(isLoser,guessedLetters.length);
+  useEffect(() => {
+    function handleKeydown(event: KeyboardEvent) {
+      const { key } = event;
+      // if (!key.match(/^[a-z]$/) || isLoser || isWinner) return;
+      if (!key.match(/^[a-z]$/) || isLoser || isWinner) return;
+      console.log("hello");
+      setGuessedLetters((p) => (p.includes(key) ? p : [...p, key]));
+    }
+
+    document.addEventListener("keydown", handleKeydown);
+
+    return () => document.removeEventListener("keypress", handleKeydown);
+  }, []);
 
   return (
     <div className="mt-7 flex flex-col items-center gap-8 mx-auto">
-      <HangmanDrawing numberOfGuesses={10} />
+      <HangmanDrawing numberOfGuesses={numberOfIncorrectGuesses} />
       <HangmanWord
         reveal={isLoser}
         wordToGuess={wordToGuess}
         guessedLetters={guessedLetters}
       />
+      <button
+        onClick={() => {
+          setGuessedLetters([]);
+          setWordToGuess(getWord());
+        }}
+        className="my-2 border-4 border-gray-700 px-6 py-1"
+      >
+        {guessedLetters.length > 0 ? "Restart" : "Next"}
+      </button>
 
-      <div style={{ alignSelf: "stretch" }}>
+      <div style={{ alignSelf: "stretch" }} className="w-[85%] mx-auto">
         <KeyBoard
-          // disabled={isWinner || isLoser}
-          disabled={false}
+          disabled={isLoser || isWinner}
           guessedLetters={guessedLetters}
           wordToGuess={wordToGuess}
           setGuessedLetters={setGuessedLetters}
         />
       </div>
-      <button
-        onClick={() => {
-          setGuessedLetters((p) => [...p, "1"]);
-        }}
-      >
-        Add
-      </button>
     </div>
   );
 }
 
 export default App;
-
-// activeLetters={guessedLetters.filter((letter) =>
-//   wordToGuess.includes(letter)
-// )}
-// inactiveLetters={incorrectLetters}
-// addGuessedLetter={addGuessedLetter}
